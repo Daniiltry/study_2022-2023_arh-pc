9c9d1d3c91b2fef214c64baabe0431985f684595
Daniiltry <Koloc04@mail.ru>
2022-12-23 20:29:06 +0300

    feat(main): add files lab-4

---
+++ labs/lab06/variant.asm
@@ -0,0 +1,34 @@
+%include 'in_out.asm'
+SECTION .data
+msg: DB 'Вариант 2: (10 + 2𝑥)/3  ',0
+gsm: DB 'Введите значение для x: ',0
+rem: DB 'Результат: ',0
+SECTION .bss
+x: RESB 80
+SECTION .text
+GLOBAL _start
+_start:
+mov eax, msg
+call sprintLF
+mov eax, gsm
+call sprintLF
+
+mov ecx, x
+mov edx, 80
+call sread
+
+mov eax,x
+call atoi
+mov ebx,2
+mul ebx
+add eax,10
+mov ebx,3
+div ebx
+mov edi,eax
+
+mov eax,rem
+call sprint
+mov eax,edi
+call iprintLF
+
+call quit
